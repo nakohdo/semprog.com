4acc18fd5937b30c88886af04d71d639eb91bb79
Frank Ralf <frank.ralf@gmx.net>
2025-02-25 17:16:53 +0100

    Add complex query for movie graph.

--- study/chapter2/chapter2-adding-querying-movie-data.py
+++ study/chapter2/chapter2-adding-querying-movie-data.py
@@ -13,7 +13,6 @@ bladerunnerActorIDs = []
 for _, _, actorID in movie_graph.triples(bladerunnerID, 'starring', None):
     bladerunnerActorIDs.append(actorID)
 
-# %%
 # Print the actors
 print(bladerunnerActorIDs)
 
@@ -63,4 +62,47 @@ movie_graph.value(None, 'starring', '/en/harrison_ford')
 # Movies with Harrison Ford, directed by Steven Spielberg
 movie_graph.query([ ('?movie', 'starring',      '/en/harrison_ford'), 
                     ('?movie', 'directed_by',   '/en/steven_spielberg')])
+
+#%% 
+# Get ID of George Lucas
+georgeLucasID = movie_graph.value(None, 'name', 'George Lucas')
+print(georgeLucasID)
+
+# %%
+# Actors in movies directed by Ridley Scott as well as movies directed by George Lucas
+movie_graph.query([ ('?mov1', 'directed_by',   '/en/ridley_scott'), 
+                    ('?mov2', 'directed_by',   '/en/george_lucas'),
+                    ('?mov1', 'starring',      '?actor'),
+                    ('?mov2', 'starring',      '?actor')    
+                ])
+# %%
+
+movie_actors = [{'?mov1': '/en/black_hawk_down',
+  '?mov2': '/authority/netflix/movie/70018728',
+  '?actor': '/en/ewan_mcgregor'},
+ {'?mov1': '/en/black_hawk_down',
+  '?mov2': '/authority/netflix/movie/70003791',
+  '?actor': '/en/ewan_mcgregor'},
+ {'?mov1': '/en/black_hawk_down',
+  '?mov2': '/authority/netflix/movie/60001814',
+  '?actor': '/en/ewan_mcgregor'},
+ {'?mov1': '/authority/imdb/title/tt0320661',
+  '?mov2': '/authority/netflix/movie/70003791',
+  '?actor': '/en/liam_neeson'},
+ {'?mov1': '/en/blade_runner',
+  '?mov2': '/en/american_graffiti',
+  '?actor': '/en/harrison_ford'},
+ {'?mov1': '/en/blade_runner',
+  '?mov2': '/authority/netflix/movie/60010932',
+  '?actor': '/en/harrison_ford'},
+ {'?mov1': '/en/blade_runner',
+  '?mov2': '/en/the_star_wars_holiday_special',
+  '?actor': '/en/harrison_ford'}]
+
+# %%
+# Get the names of the actors
+for actor in movie_actors:
+    print(movie_graph.value(actor['?actor'], 'name', None) + ': "' 
+        + movie_graph.value(actor['?mov1'], 'name', None) + '", "' 
+        + movie_graph.value(actor['?mov2'], 'name', None) +'".')
 # %%
